2fd95971c3fa26c0ae8fc4ff94d1bcbc108e7aa3
Dante Ruiz <danteruiz@me.com>
2023-12-28 13:12:52 -0600

    Update app

--- projects/capstone/starter/backend/src/app.py
+++ projects/capstone/starter/backend/src/app.py
@@ -1,4 +1,7 @@
 import os
+import logging
+from logging.handlers import RotatingFileHandler
+from flask.logging import default_handler
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -22,13 +25,33 @@ def create_app(database_path, test_config=None):
     # Check if the database needs to be initialized
     engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
     inspector = sa.inspect(engine)
-    if not inspector.has_table("users"):
+    if not inspector.has_table("movies"):
         with app.app_context():
             db.drop_all()
             db.create_all()
             app.logger.info('Initialized the database!')
     else:
-        app.logger.info('Database already contains the users table.')
+        app.logger.info('Database already contains the movies table.')
+
+    def configure_logging(app):
+        # Logging Configuration
+        if app.config['LOG_WITH_GUNICORN']:
+            gunicorn_error_logger = logging.getLogger('gunicorn.error')
+            app.logger.handlers.extend(gunicorn_error_logger.handlers)
+            app.logger.setLevel(logging.DEBUG)
+        else:
+            file_handler = RotatingFileHandler('instance/flask-talent-management.log',
+                                            maxBytes=16384,
+                                            backupCount=20)
+            file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
+            file_handler.setFormatter(file_formatter)
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        # Remove the default logger configured by Flask
+        app.logger.removeHandler(default_handler)
+
+        app.logger.info('Starting the Flask Talent Management App...')
 
 
     '''
